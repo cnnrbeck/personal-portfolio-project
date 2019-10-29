c6a1e511088b71f09f1529144d23d1191bc5ce8f
cnnrbeck <cnnrbeck@gmail.com>
2019-10-29 15:18:03 -0600

    Added buttons to the species file

--- species.js
+++ species.js
@@ -1,8 +1,9 @@
 import { species } from './assets/species.js'
 
 let mainArea = document.querySelector('main')
+let mainHeader = document.querySelector('mainHeader')
 
-species.forEach(function(species)
+species.forEach(function(race)
 {
 let speciesDiv = document.createElement('div')
 let name = document.createElement('h1')
@@ -15,7 +16,7 @@ speciesDiv.id = "speciesDiv"
 let pic = document.createElement('img')
 
 pic.setAttribute('class', 'picDivs')
-let charNum = getCharNum(species.url)
+let charNum = getCharNum(race.url)
 
 pic.src = `https://starwars-visualguide.com/assets/img/species/${charNum}.jpg`
 
@@ -25,10 +26,10 @@ speciesDiv.appendChild(language)
 speciesDiv.appendChild(lifespan)
 speciesDiv.appendChild(classification)
 
-name.textContent = "Species: " + species.name
-language.textContent = "Language: " + species.language
-lifespan.textContent = "Avg Lifespan (years): " + species.average_lifespan
-classification.innerText = "Classification: " + species.classification
+name.textContent = "Species: " + race.name
+language.textContent = "Language: " + race.language
+lifespan.textContent = "Avg Lifespan (years): " + race.average_lifespan
+classification.textContent = "Classification: " + race.classification
 
 
 mainArea.appendChild(speciesDiv)
@@ -47,3 +48,110 @@ function getCharNum(charURL){
         return charID
     }
 }
+
+const mammals = species.filter(race => race.classification === 'mammal')
+const reptiles = species.filter(race => race.classification === 'reptile')
+const amphibians = species.filter(race => race.classification === 'amphibian')
+const unknown = species.filter(race => race.classification === 'unknown')
+
+
+const allDivs = Array.from(mainArea.querySelectorAll('div'))
+
+console.log(allDivs)
+
+let mammalButton = document.createElement('button')
+mammalButton.textContent = "Mammals"
+mammalButton.addEventListener('click', () => {
+    let i = 0;
+    mammals.forEach(elt => {
+        let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification          
+        })
+        matchedDiv[i].setAttribute("style","display: visible;")
+        i++
+    }, allDivs[10].setAttribute("style", "display: visible;"))
+    i = 0
+    reptiles.forEach(elt => {
+       let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification    
+        })
+        matchedDiv[i].setAttribute("style","display: none;")
+        i++
+    })
+    i = 0
+    amphibians.forEach(elt => {
+       let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification    
+        })
+        matchedDiv[i].setAttribute("style","display: none;")
+        i++
+    })
+    i = 0
+    unknown.forEach(elt => {
+       let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification    
+        })
+        matchedDiv[i].setAttribute("style","display: none;")
+        i++
+    })
+    allDivs[0].setAttribute("style","display: none;")
+    allDivs[25].setAttribute("style","display: none;")
+    allDivs[34].setAttribute("style","display: none;")
+    allDivs[36].setAttribute("style","display: none;")
+})
+
+let nonMammalButton = document.createElement('button')
+nonMammalButton.textContent = "Non-Mammals"
+nonMammalButton.addEventListener('click', () => {
+    let i = 0;
+    mammals.forEach(elt => {
+        let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification
+           
+        })
+        matchedDiv[i].setAttribute("style","display: none;")
+        i++
+    }, allDivs[10].setAttribute("style", "display: none;"))
+    reptiles.forEach(elt => {
+        let i = 0
+        let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification
+           
+        })
+        matchedDiv[i].setAttribute("style","display: visible;")
+        i++
+    })
+    i = 0
+    amphibians.forEach(elt => {
+       let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification    
+        })
+        matchedDiv[i].setAttribute("style","display: visible;")
+        i++
+    })
+    i = 0
+    unknown.forEach(elt => {
+       let matchedDiv = allDivs.filter(element => {
+            return element.lastChild.textContent === "Classification: " + elt.classification    
+        })
+        matchedDiv[i].setAttribute("style","display: visible;")
+        i++
+    })
+    allDivs[0].setAttribute("style","display: visible;")
+    allDivs[25].setAttribute("style","display: visible;")
+    allDivs[34].setAttribute("style","display: visible;")
+    allDivs[36].setAttribute("style","display: visible;")
+})
+
+
+
+// let reptileButton = document.createElement('button')
+// reptileButton.textContent = "Reptiles"
+// reptileButton.addEventListener('click', () => {
+//     reptileButton.forEach(elt => {
+//         elt.setAttribute("style","visibility: visible")
+//     })
+// })
+
+mainHeader.appendChild(mammalButton)
+mainHeader.appendChild(nonMammalButton)
