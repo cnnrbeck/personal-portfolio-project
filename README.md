092be9909dabcfe2b77d8026df4e64c68b6add36
cnnrbeck <cnnrbeck@gmail.com>
2019-12-10 17:06:30 -0700

    fixed planet program to have an option to add planets

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# basic-javascript-practice
-Here is where I practice DOM manipulation by JS
+# Final Assignment DGM 1600
+Provides examples of Basic Javascript code including:
+    Proper use of variables with proper scope: planet.js line 18                            
+    Good use of conditional logic and value comparison: species.js line 52                  
+    Proper use of String manipulation: species.js line 29                                   
+    Good use of Arrays: species.js line 6-36                                                
+    Use of custom JavaScript objects: planets.js line 43
+Demonstrates use of ES6 including:
+    Proper use of let and const variables: species.js line 8                                
+    Use of Arrow functions: species.js line 64                                              
+    Use of Strings using Template Literals: planets.js line 50                              
+    Proper declarations of Objects: planet.js line 41
+    Use of Import and Export statements: planets.js line 1                                  
+Demonstrates use of Basic Data Structures including:
+    Using Arrays to store and manipulate collections of data: species.js line 6-36          
+    Use of Objects with key-value pairs: planet.js line 41
+    Iteration through an Array using loops and Array methods: planets.js line 18            
+Demonstrates us of Object Oriented Programming techniques including:
+    Objects with properties and methods accessed using dot notation: planet.js line 9
+    Objects using Constructors properly: planets.js line 5
--- planets.js
+++ planets.js
@@ -3,6 +3,45 @@ import { planets } from './assets/planets.js'
 
 
     let mainArea = document.querySelector('main')
+    
+    class Planet {
+        constructor (name, climate, population, yearLength, dayLength, diameter){
+                this.name = name
+                this.climate = climate
+                this.population = population
+                this.orbital_period = yearLength
+                this.rotation_period = dayLength
+                this.diameter = diameter
+            }
+        }
+
+    document.querySelector('#planetButton').addEventListener('click', () => {
+        let newName = prompt("What's the name of your planet?:")
+        let newClimate = prompt("What's the climate on your planet?:")
+        let newPop = prompt("How many people live there?:")
+        if(newPop < 0) {
+            alert('You cannot have a negative population. Try again!')
+            return
+        }
+        let newYear = prompt("How long is a year?:")
+        if(newYear < 0) {
+            alert('You cannot have a negative year length. Try again!')
+            return
+        }
+        let newDay = prompt("How long is a day?:")
+        if(newDay < 0) {
+            alert('You cannot have a negative day length. Try again!')
+            return
+        }
+        let newDiameter = prompt("Whats the diameter of the planet?:")
+        if(newDiameter < 0) {
+            alert('You cannot have a negative diameter. Try again!')
+            return
+        }
+        let newPlanet = new Planet(newName, newClimate, newPop, newYear, newDay, newDiameter)
+
+        addPlanet(newPlanet)
+    })
 
     planets.forEach(function(planet)
     {
@@ -65,3 +104,48 @@ function getCharNum(charURL){
         return charID
     }
 }
+
+function addPlanet(planet) {
+    let planetDiv = document.createElement('div')
+    let planetInfo = document.createElement('div')
+    let flipInner = document.createElement('div')
+    let flipCard = document.createElement('div')
+    let pic = document.createElement('img')
+    let name = document.createElement('h1')
+    let climate = document.createElement('h2')
+    let population = document.createElement('h2')
+    let yearLength = document.createElement('h2')
+    let dayLength = document.createElement('h2')
+    let diameter = document.createElement('h2')
+
+    planetDiv.className = "flip-card-front"
+    planetInfo.className = "flip-card-back"
+    flipInner.className = "flip-card-inner"
+    flipCard.className = "flip-card"
+
+    pic.setAttribute('class', 'picDivs')
+
+    planetDiv.appendChild(pic)
+    planetDiv.appendChild(name)
+    planetInfo.appendChild(climate)
+    planetInfo.appendChild(population)
+    planetInfo.appendChild(dayLength)
+    planetInfo.appendChild(yearLength)
+    planetInfo.appendChild(diameter)
+
+
+    pic.src="img/planet.jpg"
+
+    name.textContent = planet.name
+    climate.textContent = "climate: " + planet.climate
+    population.textContent = "population: " + planet.population
+    dayLength.textContent = "Length of Day: " + planet.rotation_period + " hrs"
+    yearLength.textContent = "Length of Year: " + planet.orbital_period + " days"
+    diameter.textContent = "Diameter: " + planet.diameter
+
+    mainArea.appendChild(flipCard)
+    flipCard.appendChild(flipInner)
+    flipInner.appendChild(planetDiv)
+    flipInner.appendChild(planetInfo)
+}
+
